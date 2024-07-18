c11500c69d439dd69c073d3252e730954e815bb5
CactusDuke <seth@sethclose.au>
2024-07-18 02:35:50 +0000

    Tests Created >Basic dataframe creation tests >Empty inputs for Pandas and Polars
    Issues
    >Pandas allows for creation of dataframes with both empty column names and None columns, frames are unaccessable and raise an error whenever attempted.
    >Polars raise an error when creation is attempted with empty column names however does not stop the creation of dataframes with None columns, same problem as Pandas where they are inaccessable.
    
    Suggestions
    >Create checks that raise ValueErrors if problamatic column names or completly empty columns are inputed

--- tests/test_dataframe_utils.py
+++ tests/test_dataframe_utils.py
@@ -1,7 +1,150 @@
 import pytest
 import pandas as pd
 import polars as pl
+import ibis
 from mountainash_data import DataFrameUtils
+from mountainash_constants import CONST_DATAFRAME_FRAMEWORK
+
+#Data Dicts
+dataDictExample1 = {
+    "column1": [1, 2, 3],
+    "column2": [4, 5, 6],
+    "column3": ["A", "B", "C"]
+}
+
+dataDictExampleEmpty = {
+    "column1": [7,8,9],
+    "column2": [2,4,6],
+    "column3": None
+}
+
+dataDictExampleDirty = {
+    "column1": 4,
+    "column2": [4.7, None, "90"],
+    "column3": ["A", 5, [5,9.7]]
+}
+
+dataDictExampleUneven = {
+    "column1": [2, 7],
+    "column2": [4.7, 8.9, 0.002],
+    "column3": ["A", "B", "C", "D", "E"]
+}
+
+# Column dictionary for renaming columns
+columnDictExample1 = {
+    "column1": "col1",
+    "column2": "col2",
+    "column3": "col3"
+}
+
+columnDictExampleEmpty = {
+    "column1": None,
+    "column2": [],
+    "column3": "col3"
+}
+
+columnDictExampleDirty = {
+    "column1": "col1",
+    "column2": " ",
+    "column3": 9
+}
+
+columnDictExampleUneven = {
+    "column1": "col1",
+    "column2": "col2"
+}
+
+# Def create_dataframe tests
+def test_create_dataframe_pandas_simple():
+    pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExample1, columnDictExample1)
+    assert isinstance(pdDataframe, pd.DataFrame)
+    assert "col3" in pdDataframe
+    assert pdDataframe["col1"][0] == 1
+    assert pdDataframe["col2"][1] == 5
+    assert pdDataframe["col3"][2] == "C"
+
+def test_create_dataframe_polas_simple():
+    plDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExample1, columnDictExample1)
+    assert isinstance(plDataFrame, pl.DataFrame)
+    assert "col3" in plDataFrame
+    assert plDataFrame["col3"][2] == "C"
+    assert plDataFrame["col1"][0] == 1
+    assert plDataFrame["col2"][1] == 5
+
+def test_create_dataframe_ibis_simple():
+    ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExample1, columnDictExample1)
+    #Expected column names
+    expCol = list(columnDictExample1.values())
+    assert ibisDataFrame.columns == expCol
+    #Expected col 1
+    expOne = dataDictExample1["column1"]
+    valuesColOne = list(ibisDataFrame.execute()["col1"])
+    assert valuesColOne == expOne
+
+
+def test_create_dataframe_pandas_empty():
+    #Pandas Empty Column Names
+    pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExample1, columnDictExampleEmpty)
+    assert isinstance(pdDataframe, pd.DataFrame)
+    with pytest.raises(TypeError):
+        print(pdDataframe)#Should not be able to be created if this breaks, which it does currently
+    #assert pdDataframe["col3"] == ["A", "B", "C"] 
+    #assert pdDataframe["column1"] == [1, 2, 3] 
+
+    #Allows for dataframe with an empty list and None to be column names. Crashes when ever attempts to use are made
+
+
+     #Pandas Empty Data
+    pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExampleEmpty, columnDictExample1)
+    print(pdDataframe)
+    with pytest.raises(ValueError): #Either the dataframe creation should be stopped before this or this should fail
+        assert pdDataframe["col1"] == [7,8,9] #Allows for creation of an unaccesable dataframe
+        assert pdDataframe["col3"] == None  
+
+
+def test_create_dataframe_polars_empty():
+
+    with pytest.raises(Exception): #Actual Polars function raises error, may be worthwhile testing in the data utils function and raising specific exception
+        plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExample1, columnDictExampleEmpty)
+    
+
+    plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExampleEmpty, columnDictExample1)
+    print(plDataframe) #Same issue as pandas, allows for creation of unaccessable dataframes with None columns
+    assert plDataframe["col1"] == [7,8,9] 
+    assert plDataframe["col3"] == None  
+
+
+"""
+
+TODO: Write empty tests for ibis dataframes
+
+"""
+
+def test_create_dataframe_ibis_empty():
+    #Ibis
+    ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExample1, columnDictExample1)
+    #Expected column names
+    expCol = list(columnDictExample1.values())
+    assert ibisDataFrame.columns == expCol
+    #Expected col 1
+    expOne = dataDictExample1["column1"]
+    valuesColOne = list(ibisDataFrame.execute()["col1"])
+    assert valuesColOne == expOne
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def test_cast_dataframe_to_pandas():
