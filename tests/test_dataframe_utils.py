6600ffe0bed3f3eaa9c5af13053888cfc05ff876
CactusDuke <seth@sethclose.au>
2024-07-22 00:44:58 +0000

    Added dirty data tests.
    Same/similar issues as empty

--- tests/test_dataframe_utils.py
+++ tests/test_dataframe_utils.py
@@ -27,7 +27,8 @@ dataDictExampleDirty = {
 dataDictExampleUneven = {
     "column1": [2, 7],
     "column2": [4.7, 8.9, 0.002],
-    "column3": ["A", "B", "C", "D", "E"]
+    "column3": ["A", "B", "C", "D", "E"],
+    "column4": [["a","b"],["c"]]
 }
 
 # Column dictionary for renaming columns
@@ -44,7 +45,7 @@ columnDictExampleEmpty = {
 }
 
 columnDictExampleDirty = {
-    "column1": "col1",
+    "column1": ["col1"],
     "column2": " ",
     "column3": 9
 }
@@ -63,6 +64,7 @@ def test_create_dataframe_pandas_simple():
     assert pdDataframe["col2"][1] == 5
     assert pdDataframe["col3"][2] == "C"
 
+
 def test_create_dataframe_polas_simple():
     plDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExample1, columnDictExample1)
     assert isinstance(plDataFrame, pl.DataFrame)
@@ -70,6 +72,7 @@ def test_create_dataframe_polas_simple():
     assert plDataFrame["col3"][2] == "C"
     assert plDataFrame["col1"][0] == 1
     assert plDataFrame["col2"][1] == 5
+
 
 def test_create_dataframe_ibis_simple():
     ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExample1, columnDictExample1)
@@ -86,15 +89,15 @@ def test_create_dataframe_pandas_empty():
     #Pandas Empty Column Names
     pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExample1, columnDictExampleEmpty)
     assert isinstance(pdDataframe, pd.DataFrame)
-    with pytest.raises(TypeError):
+
+    with pytest.raises(Exception):
         print(pdDataframe)#Should not be able to be created if this breaks, which it does currently
-    #assert pdDataframe["col3"] == ["A", "B", "C"] 
-    #assert pdDataframe["column1"] == [1, 2, 3] 
+        assert pdDataframe[None] == ["A", "B", "C"] 
 
     #Allows for dataframe with an empty list and None to be column names. Crashes when ever attempts to use are made
 
 
-     #Pandas Empty Data
+    #Pandas Empty Data
     pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExampleEmpty, columnDictExample1)
     print(pdDataframe)
     with pytest.raises(ValueError): #Either the dataframe creation should be stopped before this or this should fail
@@ -103,11 +106,11 @@ def test_create_dataframe_pandas_empty():
 
 
 def test_create_dataframe_polars_empty():
-
+    #Polars Empty Column Names
     with pytest.raises(Exception): #Actual Polars function raises error, may be worthwhile testing in the data utils function and raising specific exception
         plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExample1, columnDictExampleEmpty)
     
-
+    #Polars Empty Data
     plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExampleEmpty, columnDictExample1)
     print(plDataframe) #Same issue as pandas, allows for creation of unaccessable dataframes with None columns
     with pytest.raises(Exception):
@@ -116,11 +119,14 @@ def test_create_dataframe_polars_empty():
 
 
 def test_create_dataframe_ibis_empty():
+    #Ibis Empty Column Names
     with pytest.raises(Exception): 
         ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExample1, columnDictExampleEmpty)
 
+    #Ibis Empty Data
     ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExampleEmpty, columnDictExample1)
     print(ibisDataFrame)
+
     #Expected column names
     expCol = list(columnDictExample1.values())
     assert ibisDataFrame.columns == expCol
@@ -130,6 +136,118 @@ def test_create_dataframe_ibis_empty():
     #Ibis actually works after being created with an empty value for one of the columns, needs to be restricted if we want it
     #to stay in line with Polars and Pandas.
     #Still need to raise value error when created with empty column names in the creation function
+
+
+def test_create_dataframe_pandas_dirty():
+    #Pandas Dirty Column Names
+    pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExample1, columnDictExampleDirty)
+    assert isinstance(pdDataframe, pd.DataFrame)
+    with pytest.raises(Exception):
+        print(pdDataframe) #Allows creation with lists as column values. Crashes
+    assert " " in pdDataframe
+    assert pdDataframe[9][0] == "A"
+    #Empty space strings and integers work as column names
+
+
+     #Pandas Dirty Data
+    pdDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.PANDAS.value, dataDictExampleDirty, columnDictExample1)
+    print(pdDataframe)
+    assert pdDataframe["col1"][0] == 4
+    with pytest.raises(Exception):
+        assert pdDataframe["col1"] == 4 #When only one value is inputed Pandas breaks trying to find the entire column
+    assert pdDataframe["col2"][2] == "90" 
+    assert pdDataframe["col3"][1] == 5 
+    assert pdDataframe["col3"][2][1] == 9.7 
+
+    #Need to ensure that inputed data is in a list form. Could just put the value in a list and multiple by row number
+
+
+def test_create_dataframe_polars_dirty():
+    #Polars Dirty Column Names
+    with pytest.raises(Exception):
+        plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExample1, columnDictExampleDirty)
+
+        assert isinstance(plDataFrame, pl.DataFrame)
+        assert "col1" in plDataFrame
+        assert " " in plDataFrame
+        assert 9 in plDataFrame
+        assert plDataFrame["col1"][2] == "1"
+        assert plDataFrame[" "][0] == 1
+        assert plDataFrame[9][1] == "B"
+
+    #Cannot create dataframe with a list as column name, error raised not in function so not that helpful for debugging later
+
+    #Polars Dirty Data
+    plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, dataDictExampleDirty, columnDictExample1)
+    #Same issue as pandas, allows for creation of unaccessable dataframes with None columns
+    assert plDataframe["col1"][0] == 4
+    with pytest.raises(Exception):
+        assert plDataframe["col1"] == 4 #Does same as Pandas
+    #Intrestling Polars removes the differing data types, leaves Null for everthing except strings    
+    assert plDataframe["col2"][0] == None
+    assert plDataframe["col2"][2] == "90"
+    assert plDataframe["col3"][2] == None 
+    assert plDataframe["col3"][0] == "A"
+
+    #Dirty no strings
+    tempDict = {}
+    tempDict["column1"] = [4, 4, 4]
+    tempDict["column2"] = [1, 2.4, 3]
+    tempDict["column3"] = [1.2, 2.3, "3.4"]
+
+    plDataframe = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.POLARS.value, tempDict, columnDictExample1)   
+    assert plDataframe["col1"][0] == 4
+    assert plDataframe["col2"][0] == 1
+    assert plDataframe["col2"][1] == 2.4
+    assert plDataframe["col3"][2] == "3.4" 
+    assert plDataframe["col3"][0] == 1.2
+
+    #Polars always chooses the string as the data type when a column has more than one, this is different than pandas
+    #Need to validate that all inputed values are the same type in creation
+    #If integers and floats are inputted it changes the integers into float values
+
+
+def test_create_dataframe_ibis_dirty():
+    with pytest.raises(Exception):
+        ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExample1, columnDictExampleDirty)
+    #Cannot create column names with lists, need to move exception raising to function
+
+    ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, dataDictExampleDirty, columnDictExample1)
+    valuesColOne = list(ibisDataFrame.execute()["col1"])
+    valuesColTwo = list(ibisDataFrame.execute()["col2"])
+    valuesColThree = list(ibisDataFrame.execute()["col3"])
+    assert valuesColOne == [4] * 3 #Ibis actually works with a single value inputted, creates a list so each row has the value
+    assert valuesColTwo == [None, None, "90"]
+    assert valuesColThree == ["A", None, None]
+    #Does the same as Polars, if multiple data type are inputted it defaults to strings
+
+    #Dirty no strings
+    tempDict = {}
+    tempDict["column1"] = [4, 4, 4]
+    tempDict["column2"] = [1, 2.4, 3]
+    tempDict["column3"] = [1.2, 2.3, "3.4"]
+
+    ibisDataFrame = DataFrameUtils.create_dataframe(CONST_DATAFRAME_FRAMEWORK.IBIS.value, tempDict, columnDictExample1)
+    valuesColOne = list(ibisDataFrame.execute()["col1"])
+    valuesColTwo = list(ibisDataFrame.execute()["col2"])
+    valuesColThree = list(ibisDataFrame.execute()["col3"])
+
+    assert valuesColOne == [4] * 3 
+    assert valuesColTwo == [1, 2.4, 3]
+    assert valuesColThree == [None, None, "3.4"]
+    #Same as Polars
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
