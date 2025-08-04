8652d435dfa0fb9b2ffd62bdb5c34d0a5c762149
Nathaniel Ramm <nathaniel.ramm@discretedatascience.com>
2025-08-05 00:05:41 +1000

    🔥 Remove dataframes module and migrate to mountainash-dataframes package
    - Remove entire dataframes module and utilities
    - Remove associated test files for dataframes functionality
    - Remove ibis_connection_factory.py (empty file)
    - Dataframes functionality now provided by separate mountainash-dataframes dependency

--- src/mountainash_data/dataframes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base_dataframe import BaseDataFrame
-from .ibis_dataframe import IbisDataFrame, init_ibis_connection
-
-__all__ = (
-    "BaseDataFrame",
-    "IbisDataFrame",
-    "init_ibis_connection"
-)
